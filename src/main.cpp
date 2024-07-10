58f958d15380e453dcffd830aaa4429474357566
knappyflash <knappyflash2@gmail.com>
2024-07-09 19:34:32 -0700

    Almost Done

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
 #include <Arduino.h>
+#include <cstdlib> // for rand() and srand()
+#include <ctime>   // for time()
 #include <MyDisplay.cpp>
 #include <MyThermometer.cpp>
+#include <SplashArrayForJosh.h>
 // #include <MyWifi.cpp>
 
 MyDisplay MyDisplay1;
@@ -23,39 +26,39 @@ const long interval1000 = 1000; // 1 seconds
 const long interval30000 = 30000; // 30 seconds
 const long interval60000 = 60000; // 1 minutes
 void Function_Interval_1_Seconds() {
-    if (!MyThermometer1.Get_isConnected()) {
-        MyThermometer1.Connect("a4:c1:38:65:97:23");
-    }
-
-    if (!MyThermometer2.Get_isConnected()) {
-        MyThermometer2.Connect("a4:c1:38:42:97:18");
-    }
 }
 void Function_Interval_30_Seconds() {
-    Serial.println("Every 30 Seconds:");
 }
 void Function_Interval_1_Minute() {
 
-    Serial.println("Every Minute:");
+    if (!MyThermometer1.Get_isConnected()){
+        MyThermometer1.Connect("a4:c1:38:65:97:23");
+    }
+    if (!MyThermometer2.Get_isConnected()){
+        MyThermometer2.Connect("a4:c1:38:42:97:18");
+    }
 
-    Serial.print("Free heap memory: ");
-    Serial.print(ESP.getFreeHeap());
-    Serial.println(" bytes");
+    if (MyThermometer1.Get_isConnected()){
+        Temp1 = MyThermometer1.Get_Temperature();
+        Humty1 = MyThermometer1.Get_Humidity();
+        Battery1 = MyThermometer1.Get_BatteryLvl();
+    }
 
-    Temp1 = MyThermometer1.Get_Temperature();
-    Humty1 = MyThermometer1.Get_Humidity();
-    Battery1 = MyThermometer1.Get_BatteryLvl();
+    if (MyThermometer2.Get_isConnected()){
+        Temp2 = MyThermometer2.Get_Temperature();
+        Humty2 = MyThermometer2.Get_Humidity();
+        Battery2 = MyThermometer2.Get_BatteryLvl();
+    }
 
-    Temp2 = MyThermometer2.Get_Temperature();
-    Humty2 = MyThermometer2.Get_Humidity();
-    Battery2 = MyThermometer2.Get_BatteryLvl();
-    
-    Serial.print("Temp1: ");
-    Serial.print(Temp1);
-    Serial.print(" Temp2:");
-    Serial.println(Temp2);
+    if (MyThermometer1.Get_isConnected() && MyThermometer2.Get_isConnected()){
+        MyDisplay1.Do_A_Loop(Temp1, Temp2, Humty1, Humty2, Battery1, Battery2,SplashArrayForJosh[random(0, 551)]);
+    }else{
+        MyDisplay1.Do_A_Loop(-1, -1, -1, -1, -1, -1,"Hi Josh. Nothing To See Here...");
+    }
 
-    MyDisplay1.Do_A_Loop(Temp1, Temp2, Humty1, Humty2, Battery1, Battery2);
+    // Serial.print("Free heap memory: ");
+    // Serial.print(ESP.getFreeHeap());
+    // Serial.println(" bytes");
     
 }
 
